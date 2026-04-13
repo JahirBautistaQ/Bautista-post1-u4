5697b61f9e4c6336c4491d343334cca45cc44df2
JahirBautistaQ <02210132008@mail.udes.edu.co>
2026-04-13 10:53:48 -0500

    Proyecto base Spring Boot

---
+++ src/main/java/com/universidad/pedidos_comportamiento/PedidosComportamientoApplication.java
@@ -0,0 +1,13 @@
+package com.universidad.pedidos_comportamiento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosComportamientoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidosComportamientoApplication.class, args);
+	}
+
+}

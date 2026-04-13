b7aded9d7dcac268365c1ac19f98878abcec47c2
JahirBautistaQ <02210132008@mail.udes.edu.co>
2026-04-13 11:04:50 -0500

    Creacion de la estructura del proyecto y agregado del pom.xml

---
+++ src/main/java/com/universidad/pedidos/PedidosApp.java
@@ -0,0 +1,12 @@
+package com.universidad.pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PedidosApp.class, args);
+    }
+}
